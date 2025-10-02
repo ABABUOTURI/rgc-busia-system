16df377511d0da9b0fd193e07bfdc5d5d8cbb84e
Legend <ababuoturi@gmail.com>
2025-10-03 00:44:44 +0300

    snr pastors-Announcements and Dashboards

--- src/models/ChurchAccount.ts
+++ src/models/ChurchAccount.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const HBCSchema = new Schema({
   jerusalem: { type: Number, default: 0 },
